69dcc1d0a2f5b83a759302a82b2636814aca36d2
RecursiveByte <opboy4885@gmail.com>
2025-01-29 23:51:54 +0530

    Update server.js

--- PassWordManager/backEnd/server.js
+++ PassWordManager/backEnd/server.js
@@ -14,7 +14,7 @@ app.use(cors())
 dotenv.config()
 
 // Connection URL
-const url = 'mongodb+srv://Joyboy:kNhyZMPw6oD4q2Kp@cluster0.tzjeg.mongodb.net/keyGuardian?retryWrites=true&w=majority';
+const url = 'mongodb+srv://Joyboy:kNhyZMPw6oD4q2Kp@cluster0.tzjeg.mongodb.net/keyGuardian?retryWrites=true&w=majority&tls=true';
 const client = new MongoClient(url);
 
 client.connect().then(()=>console.log("connected"));
