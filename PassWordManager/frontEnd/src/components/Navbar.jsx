bdf6878e57f8ae37f8626a9a6b6dbae8a9c52376
RecursiveByte <opboy4885@gmail.com>
2025-02-02 15:31:55 +0530

    Update Navbar.jsx

--- PassWordManager/frontEnd/src/components/Navbar.jsx
+++ PassWordManager/frontEnd/src/components/Navbar.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
     <>
-      <nav className='w-[100vw] h-[8vh]  text-white  bg-black flex justify-between'>
-        <div className='my-auto mx-4 font-bold'>
-        <span className=' text-blue-500'>Key</span>
-        <span >Guardian</span>
+      <nav className="w-[100vw] h-[8vh] text-white bg-black flex justify-between items-center px-4">
+        <div className="w-[30%] h-full font-bold  flex items-center px-2">
+          <span className="text-blue-500">Key</span>
+          <span>Guardian</span>
         </div>
-        
-        
-        <ul className='flex gap-10 w-[20%] h-[100%]  text-blue-500 justify-center items-center text-xl '>
-          <li><a>Home</a></li>
-          <li><a>contact</a></li>
-          <li><a>About</a></li>
-        </ul>
+
+      
+        <div className="flex w-[35%] h-full  text-blue-500 items-center text-lg sm:text-xl">
+          <ul className="flex w-full justify-evenly items-center flex-wrap">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </div>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
