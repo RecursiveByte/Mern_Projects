aea97a1f7881a1cb3f96423b25c11f9ae8cdb5d6
RecursiveByte <opboy4885@gmail.com>
2025-05-11 02:04:57 +0530

    Update authController.js

--- PasswordManager_2.0/backend/controller/authController.js
+++ PasswordManager_2.0/backend/controller/authController.js
@@ -64,8 +64,8 @@ export const login = async (req, res) => {
             res.cookie("token1", token,
                 {
                     maxAge: 1 * 24 * 60 * 60 * 1000,
-                    httpOnly: true,
-                    secure: true,
+                    httpOnly: false,
+                    secure: false,
                     sameSite: "none",
                   
                 }
