b81baa233632cd5e3d5a2ff36df67eae5ecdccc5
RecursiveByte <opboy4885@gmail.com>
2025-05-11 03:03:35 +0530

    Update authController.js

--- PasswordManager_2.0/backend/controller/authController.js
+++ PasswordManager_2.0/backend/controller/authController.js
@@ -27,9 +27,9 @@ export const register = async (req, res) => {
     res.cookie("token1", token,
         {
             maxAge: 1 * 24 * 60 * 60 * 1000,
-            httpOnly: false,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? "strict" : "lax"
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
         }
     )
 
@@ -85,15 +85,15 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
     res.clearCookie("token1", {
-        httpOnly: false,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? "strict" : "lax"
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
     });
 
     res.clearCookie("token2", {
-        httpOnly: false,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? "strict" : "lax"
+       httpOnly: true,
+        secure: true,
+        sameSite: "none"
     });
 
     res.json({ success: true, message: "logout successfully" });
