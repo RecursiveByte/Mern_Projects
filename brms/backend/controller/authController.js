84f6b8213bd0f5912658237b139d962e4d4714e0
RecursiveByte <opboy4885@gmail.com>
2025-08-03 03:52:01 +0530

    Update authController.js

--- brms/backend/controller/authController.js
+++ brms/backend/controller/authController.js
@@ -169,7 +169,7 @@ export const googleLogin = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000
     });
 
@@ -407,7 +407,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'Strict',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000
     });
 
