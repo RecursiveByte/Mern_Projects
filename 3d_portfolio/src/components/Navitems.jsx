ae9a1bd7d0fa90057f20e6af7ddfed238973cec0
RecursiveByte <opboy4885@gmail.com>
2025-08-17 02:46:30 +0530

    Update Navitems.jsx

--- 3d_portfolio/src/components/Navitems.jsx
+++ 3d_portfolio/src/components/Navitems.jsx
@@ -5,9 +5,29 @@ import { Link } from "react-scroll";
 const Navitems = () => {
   return (
     <>
-            {Navlinks.map((ele) => (
-                <li key={ele.id} className= 'text-gray-400 hover:text-white'> <Link to={ele.to} spy={true} smooth={true} duration={300} >{ele.name}  </Link></li>
-            ))}
+      {Navlinks.map((ele) => {
+        if(ele.id === 0) {
+          return (
+            <li key={ele.id} className='text-gray-400 hover:text-white'>
+              <a 
+                href="https://drive.google.com/file/d/1IV-ZLCJKELL_8Zf-CR3-VRKtqLVF7iuN/view?usp=sharing" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className='cursor-pointer'>
+                {ele.name}
+              </a>
+            </li>
+          )
+        } else {
+          return (
+            <li key={ele.id} className='text-gray-400 hover:text-white'>
+              <Link to={ele.to} spy={true} smooth={true} duration={300}>
+                {ele.name}
+              </Link>
+            </li>
+          )
+        }
+      })}
     </>
   )
 }
